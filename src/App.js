6f9f617554e2952f1de8d07d150281c313f39bd7
Tawnie Bavaro <tawniebavaro@gmail.com>
2020-10-11 14:52:51 +0100

    move handleSubmit

--- src/App.js
+++ src/App.js
@@ -1,50 +1,43 @@
 import React, { useState } from "react";
 import { parseString } from "xml2js";
+import "./App.css";
 
 function Upload() {
   const handleSubmit = (event) => {
     event.preventDefault();
-    const files = event.target.files[0];
-    console.log("files", files);
-    fetch("https://fanvue-intervue.s3-eu-west-1.amazonaws.com/files.name", {
+    const files = event.target[0].files[0];
+    console.log("files", files.name);
+    fetch(`https://fanvue-intervue.s3-eu-west-1.amazonaws.com/${files.name}`, {
       method: "PUT",
       body: {
         file: files,
       },
     })
       .then((response) => response.json())
-      .then((data) => console.log(data))
       .catch((error) => {
         console.log(error);
       });
   };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>
-        Upload File;
-        <input type="file" onChange={handleSubmit} />
+        Upload File
+        <input type="file" />
       </label>
       <button type="submit">Upload</button>
     </form>
   );
 }
 
-const Image = ({ url }) => {
-  return (
-    <div>
-      <img src={url} alt="Sorry! Not here" />
-    </div>
-  );
-};
-
 const ImageContainer = ({ images }) => {
   return (
     <div>
       {images.map((image, index) => (
-        <Image
-          url={`https://fanvue-intervue.s3-eu-west-1.amazonaws.com/${image.Key}`}
+        <img
+          src={`https://fanvue-intervue.s3-eu-west-1.amazonaws.com/${image.Key}`}
           key={index}
+          alt="Sorry!"
         />
       ))}
     </div>
